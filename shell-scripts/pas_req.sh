9030d43681452dbe6a933082ed3e1a55bbf53776
clicktripz <l87dg5f1!>
2020-06-28 14:10:40 -0700

    Refactoring

--- shell-scripts/pas_req.sh
+++ shell-scripts/pas_req.sh
@@ -1,24 +1,6 @@
 #/bin/bash
 
-url_encode() {
-  local string="${1}"
-  local strlen=${#string}
-  local encoded=""
-  local pos c o
-
-  for (( pos=0 ; pos<strlen ; pos++ )); do
-     c=${string:$pos:1}
-     case "$c" in
-        [-_.~a-zA-Z0-9] ) o="${c}" ;;
-        * )               printf -v o '%%%02x' "'$c"
-     esac
-     encoded+="${o}"
-  done
-  echo "${encoded}"
-  REPLY="${encoded}"
-}
-
-function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+. $(dirname "$0")/functions/base.sh
 
 # ##############################################################################################################
 
