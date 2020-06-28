55fe6d435521e9ebf3530e8a12b7d057af439e39
clicktripz <l87dg5f1!>
2020-06-28 14:09:02 -0700

    Refactoring

---
+++ shell-scripts/functions/base.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+url_encode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"
+  REPLY="${encoded}"
+}
+
+function join_by { 
+    local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; 
+}
+
