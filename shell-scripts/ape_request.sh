62f111c4d8eada1aa4c2a77564a9dcc66dbb5410
clicktripz <l87dg5f1!>
2020-08-11 23:02:41 -0700

    comments

--- shell-scripts/ape_request.sh
+++ shell-scripts/ape_request.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://admin.clicktripz.com/api/admin/v1/advertiser/audience/pricing?advertiserId=462
+# https://admin.clicktripz.com/api/rte/v1/users/d83dc1ae-0126-47ac-9231-11aed27c6ba7/profile?advertisers=462
+
+
 ape_service="https://localhost:8888/v1/audience_pricing"
 
 curl -k --request POST ${ape_service}  \
