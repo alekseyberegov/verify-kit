080662e1b0c5957513a109c5536063c2eab8667a
clicktripz <l87dg5f1!>
2020-12-04 23:39:08 -0800

    Add CRS testing

---
+++ shell-scripts/crs_req.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+sample_size=100
+temp_file=$(mktemp)
+publisher_id="3158"
+experiment_name="crs_split_test"
+
+usage ()
+{
+   printf -- "\n"
+   printf -- "Usage: \n"
+   printf -- "  $0 [OPTIONS]\n\n"
+   printf -- "Send request to CRS\n\n"
+   printf -- "Options:\n"
+   printf -- "  -s, --size          the sample size\n"
+   printf -- "  -e, --experiment    the experiment name\n"
+   printf -- "  -p, --publisher_id  the publisher ID\n"
+   printf -- "  -h, --help          show help\n"
+   printf -- "\n"
+   exit 1
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+   key="$1"
+
+   case $key in
+      -h|--help)
+      usage
+      ;;
+      -s|--size)
+      sample_size=$2
+      shift
+      shift 
+      ;;
+      -e|--experiment)
+      experiment_name=$2
+      shift
+      shift 
+      ;;
+      -p|--publisher_id)
+      publisher_id=$2
+      shift
+      shift 
+      ;;
+      *)
+      POSITIONAL+=("$1") 
+      shift 
+      ;;
+   esac
+done
+set -- "${POSITIONAL[@]}"
+
+echo $temp_file
+
+for i in $(seq 1 ${sample_size});
+do
+    user_id=$(uuidgen)
+
+    curl -s -X POST "http://localhost:8080/api/v1/resolve" \
+        -H "accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "cookie: _ctuid=${user_id};" \
+        -d "{ \"publisher_id\": \"${publisher_id}\", \"experiment\": { \"experiment\": \"${experiment_name}\"}}" | \
+    python3 -c "import sys, json; print(json.load(sys.stdin)['data']['creative_id'])" >> ${temp_file}
+done
+
+close_cnt=$(grep "close-creative" ${temp_file} | wc -l)
+printf -- "%s from %s are no-show creatives, percentage = %s%%\n" \
+    ${close_cnt} ${sample_size}  $(echo "scale=2 ; 100 * ${close_cnt} / ${sample_size}" | bc) 
+
+rm ${temp_file}
